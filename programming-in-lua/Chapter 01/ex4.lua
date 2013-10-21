06b2a7f6ee442a9608b6985f83b7226e6e651378
Ivan Bobev <bobeff@gmail.com>
2013-10-22 00:57:51 +0300

    Exercise 1.4

---
+++ programming-in-lua/Chapter 01/ex4.lua
@@ -0,0 +1,21 @@
+--[[
+	Which of the following strings are valid identifiers?
+]]
+
+___ = "___"
+print(___)
+
+_end = "_end"
+print(_end)
+
+End = "End"
+print(End)
+
+-- end == "end"      -- impossible, end is a keyword
+
+-- until? = "until?" -- impossible, identifier cannot contain a ?
+
+-- nil = "nil"       -- impossible, nil is a keyword
+
+NULL = "NULL"
+print(NULL)
