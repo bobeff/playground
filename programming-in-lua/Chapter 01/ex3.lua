1e0192dbc1c25a04dd863979b90d2b6a76717918
Ivan Bobev <bobeff@gmail.com>
2013-10-22 00:57:17 +0300

    Exercise 1.3

---
+++ programming-in-lua/Chapter 01/ex3.lua
@@ -0,0 +1,13 @@
+--[[
+	Can you name other languages that use -- for comments?
+]]
+
+-- from wikipedia
+print("Euphoria")
+print("Haskell")
+print("SQL")
+print("Ada")
+print("AppleScript")
+print("Eiffel")
+print("VHDL")
+print("SGML")
