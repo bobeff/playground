07b2fa4f99739aa8807a51661acb5ccdb13cfd85
Ivan Bobev <bobeff@gmail.com>
2013-10-24 02:26:13 +0300

    Exercise 3.2

---
+++ programming-in-lua/Chapter 03/ex2.lua
@@ -0,0 +1,10 @@
+--[[
+	What is the result of expression 2 ^ 3 ^ 4?
+What about 2 ^ -3 ^ 4 ?
+]]
+
+print(2 ^ 3 ^ 4)
+print(2 ^ 3 ^ 4 == 2 ^ (3 ^ 4))
+
+print(2 ^ -3 ^ 4)
+print(2 ^ -3 ^ 4 == 2 ^ (-(3 ^ 4)))
