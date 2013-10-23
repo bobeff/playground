fdc7405a5e2cb6add8ea869b244833f74b8bcade
Ivan Bobev <bobeff@gmail.com>
2013-10-23 03:09:11 +0300

    Exercise 2.1

---
+++ programming-in-lua/Chapter 02/ex1.lua
@@ -0,0 +1,11 @@
+--[[
+	What is the value of expression type(nil) == nil? (You can use Lua to
+check your answer.) Can you explin this result?
+]]
+
+print(type(nil) == nil)
+
+--[[
+	The result of "type(nil)" is string nil which is different from constant
+nil of type nil.
+]]
