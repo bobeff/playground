349776f5c9970879932e0a6cc47dfff6f5cb22ec
Ivan Bobev <bobeff@gmail.com>
2013-10-23 03:09:25 +0300

    Exercise 2.2

---
+++ programming-in-lua/Chapter 02/ex2.lua
@@ -0,0 +1,16 @@
+--[[
+	Which of the following are valid numbers? What are their values?
+]]
+
+print(.0e12)
+-- print(.e12)
+-- print(0.0e)
+print(0x12)
+--print(0xABFG)
+print(0xA)
+--print(FFFF)
+print(0xFFFFFFFF) -- 2 ^ 32 - 1
+--print(0x)
+print(0x1P10) -- 1 * 2 ^ 10
+print(0.1e1)
+print(0x0.1p1) -- (1 * 16 ^ -1) * 2 ^ 1
