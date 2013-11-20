0050d86eda403e23b407450f3bffafd807b215fd
Ivan Bobev <bobeff@gmail.com>
2013-11-20 12:16:09 +0200

    Exercise 17.2

---
+++ programming-in-lua/Chapter 17/ex2.1.lua
@@ -0,0 +1,10 @@
+--[[
+    Consider the first example of Section 17.6, which creates a table with a
+finalizer that only prints a message when activated. What happens if the
+program ends without a collection cycle ? What happens if program calls
+os.exit ? What happens if the program ends with some error ?
+]]
+
+o = {x = "hi"}
+setmetatable(o, { __gc = function(o) print(o.x) end })
+o = nil
