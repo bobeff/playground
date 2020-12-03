c29ea03bb6ae3083036dac84194ecaca6134b8ce
Ivan Bobev <bobeff@protonmail.ch>
2020-12-03 11:32:17 +0200

    Add solutions to AoC 2020 day 3 problems

---
+++ advent-of-code/2020/03/toboggan_trajectory.nim
@@ -0,0 +1,26 @@
+import strutils
+
+proc readInput(): seq[string] =
+  for line in "input".lines:
+    result.add line.strip
+
+proc solve(slope: seq[string], slopeX, slopeY: int): uint =
+  let
+    width = slope[0].len
+    height = slope.len
+  var currentX, currentY = 0
+  while currentY < height:
+    if slope[currentY][currentX] == '#':
+      result.inc
+    currentX = (currentX + slopeX) mod width
+    currentY += slopeY
+
+let input = readInput()
+let slope1x1 = solve(input, 1, 1)
+let slope3x1 = solve(input, 3, 1)
+let slope5x1 = solve(input, 5, 1)
+let slope7x1 = solve(input, 7, 1)
+let slope1x2 = solve(input, 1, 2)
+
+echo slope3x1
+echo slope1x1 * slope3x1 * slope5x1 * slope7x1 * slope1x2
