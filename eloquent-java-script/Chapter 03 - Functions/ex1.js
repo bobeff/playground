06b944bfae7553edd75f486fae2f3d0ffe20c0d9
Ivan Bobev <bobeff@protonmail.ch>
2024-03-02 18:16:24 +0200

    Add "Eloquent JavaScript" - Chapter 3 exercises solutions

---
+++ eloquent-java-script/Chapter 03 - Functions/ex1.js
@@ -0,0 +1,14 @@
+/*
+Minimum
+
+The previous chapter introduced the standard function Math.min that returns its
+smallest argument. We can write a function like that ourselves now. Define the
+function min that takes two arguments and returns their minimum.
+*/
+
+function min(x, y) {
+  return x < y ? x : y;
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
---
+++ eloquent-java-script/Chapter 03 - Functions/ex2.js
@@ -0,0 +1,36 @@
+/*
+Recursion
+
+We’ve seen that we can use % (the remainder operator) to test whether a number
+is even or odd by using % 2 to see whether it’s divisible by two. Here’s another
+way to define whether a positive whole number is even or odd:
+
+- Zero is even.
+- One is odd.
+- For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. The
+function should accept a single parameter (a positive, whole number) and return
+a Boolean.
+
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to
+fix this?
+*/
+
+function isEven(n) {
+  n = Math.abs(n);
+  if (n === 0) {
+    return true;
+  }
+  else if (n === 1) {
+    return false;
+  }
+  else {
+    return isEven(n - 2);
+  }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-2));
